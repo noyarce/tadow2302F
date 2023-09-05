20178a83dce9691c1579cc2616087ee6cf664516
Nicolas Oyarce V <nicolas.oyarce@mundotelecomunicaciones.cl>
2023-09-05 19:06:46 -0300

    wip

--- src/Pages/Components/Pokemon.jsx
+++ src/Pages/Components/Pokemon.jsx
@@ -1,24 +1,47 @@
-import { Divider, List, ListItem, ListItemText } from "@mui/material";
+import { Divider, Grid, List, ListItem, ListItemText } from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
 export default function Pokemon() {
   const [pokemones, setPokemones] = useState([]);
+
+  const [buscador, setBuscador] = useState("");
+  const [listaAux, setListaAux] = useState([]);
+  
   function cargarListado() {
     axios
-      .get("https://pokeapi.co/api/v2/pokemon?limit=2000")
+      .get("https://pokeapi.co/api/v2/pokemon?limit=151")
       .then((response) => {
         setPokemones(response.data.results);
       });
   }
-  console.log(pokemones);
 
   useEffect(() => {
     cargarListado();
   }, []);
 
+
+const handleInputChange = (event) => {
+    const { name, value } = event.target;
+   setBuscador(value);
+  };
+
+  useEffect(() => {
+    if (buscador.trim() !== "") {
+      let result = pokemones.filter((item) =>
+        item.name.toString().includes(buscador.toString().trim())
+      );
+      setListaAux(result);
+    } else {
+      setListaAux([]);
+    }
+  }, [buscador]);
+
   return (
     <>
+    <input name="buscador" onChange={handleInputChange}></input>
+      <Grid container spacing ={1}>
+      <Grid item md={6}>
       <List>
       {pokemones.map((item, index) => (
       <> 
@@ -32,6 +55,27 @@ export default function Pokemon() {
        
        
       </List>
+      </Grid>
+
+      <Grid item md={6}>
+      <List>
+      {listaAux.map((item, index) => (
+      <> 
+      <ListItem disablePadding key={index}>
+          <ListItemText primary={item.name} />
+        </ListItem>
+        <Divider></Divider>
+      </>
+        
+      ))}
+       
+       
+      </List>
+      </Grid>
+      </Grid>
+      
+
+      
     </>
   );
 }
