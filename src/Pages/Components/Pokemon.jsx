3f42673d03845063fb462c2b7df14b678eee2c8f
Nicolas Oyarce V <nicolas.oyarce@mundotelecomunicaciones.cl>
2023-09-05 18:29:43 -0300

    wip

--- src/Pages/Components/Pokemon.jsx
+++ src/Pages/Components/Pokemon.jsx
@@ -1,30 +1,38 @@
-import axios from "axios"
-import { useEffect , useState} from "react"
+import { Divider, List, ListItem, ListItemText } from "@mui/material";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-export default function Pokemon(){
-    const [pokemones, setPokemones]=useState([]);
-    function cargarListado(){
-        axios.get('https://pokeapi.co/api/v2/pokemon?limit=2000').then(
-            (response)=>{
-                setPokemones(response.data.results)
+export default function Pokemon() {
+  const [pokemones, setPokemones] = useState([]);
+  function cargarListado() {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon?limit=2000")
+      .then((response) => {
+        setPokemones(response.data.results);
+      });
+  }
+  console.log(pokemones);
 
-            }
-        )
-    }
-                console.log(pokemones)
+  useEffect(() => {
+    cargarListado();
+  }, []);
 
-    useEffect(()=>{
-        cargarListado();
-    },[]);
-
-
-    return(<>
-
-    {pokemones.map((item, index) => 
-    (
-       <>{item.name}</> 
-    )
- )}
-    Hola que tal 
-    </>)
+  return (
+    <>
+      <List>
+      {pokemones.map((item, index) => (
+      <> <ListItem disablePadding>
+          <ListItemText primary={item.name} />
+        </ListItem>
+        <Divider></Divider>
+      </>
+        
+      ))}
+       
+       
+      </List>
+      
+      Hola que tal
+    </>
+  );
 }
